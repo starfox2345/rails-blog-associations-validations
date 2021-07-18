e460f5296cc3391760be09db6735178519b71e3c
yatsun <yatsunlam25@gmail.com>
2021-07-18 17:34:35 -0400

    Done.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
-    has_many :posts
-    has_many :users, through: :posts
+    has_many :post_tags
+    has_many :posts, through: :post_tags
     validates :name, uniqueness: true
 end
